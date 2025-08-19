a990e4c4f57b0b2a7c23071f58d50baf6736e4e2
Dargreat <132403953+Dargreat@users.noreply.github.com>
2025-08-19 11:57:05 +0100

    Update route.js

--- app/api/callback/route.js
+++ app/api/callback/route.js
@@ -1,4 +1,4 @@
-import { setAccessToken } from '@/lib/tokenStore';
+import { setAccessToken } from '../../../lib/tokenStore';
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
